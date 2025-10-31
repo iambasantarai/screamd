cb97e61b6389c9e1e690201b9dd18de213338f64
iambasantarai <developer.basanta@gmail.com>
2025-10-31 14:17:24 +0545

    Execute external command 'aplay' to play sound

--- main.c
+++ main.c
@@ -4,5 +4,10 @@
 int main() {
     printf("screamd: daemon to make pc scream when memory < 1024MiB\n");
 
+    int status = system("aplay scream.wav1");
+    if(status != 0){
+        exit(EXIT_FAILURE);
+    }
+
     exit(EXIT_SUCCESS);
 }
