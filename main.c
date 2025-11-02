5a884b7c53076c1bc9852b8943f65d9de62170bb
iambasantarai <developer.basanta@gmail.com>
2025-11-02 19:50:14 +0545

    Play sound if available memory < threshold

--- main.c
+++ main.c
@@ -2,8 +2,43 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <unistd.h>
+
+#define BUF_SIZE 128
+#define MIN_MEM_THRES_IN_MIB 1024
+
+void scream() {
+  char *cmd = "aplay -D sysdefault scream.wav";
+
+  int result = system(cmd);
+  if (result != 0) {
+    fprintf(stderr, "Error playing sound: system() returned %d\n", result);
+  }
+}
+
+int get_avail_memory() {
+  char *cmd = "free -m | grep 'Mem:' | awk '{print $4}'";
+  int avail_mem = 0;
+
+  char mem_info_buf[BUF_SIZE] = {0};
+  FILE *fp;
+
+  if ((fp = popen(cmd, "r")) == NULL) {
+    printf("Error opening pipe.\n");
+    return EXIT_FAILURE;
+  }
+
+  while (fgets(mem_info_buf, BUF_SIZE, fp) != NULL) {
+    avail_mem = atoi(mem_info_buf);
+  }
+
+  if (pclose(fp)) {
+    printf("Error closing pipe.");
+    return EXIT_FAILURE;
+  }
+
+  return avail_mem;
+}
 
 int main(void) {
   pid_t child_pid = fork();
@@ -34,11 +69,14 @@ int main(void) {
   dup2(log_fd, STDOUT_FILENO);
   dup2(log_fd, STDERR_FILENO);
 
+  printf("Starting daemon...\n");
   while (1) {
-    time_t now = time(NULL);
-    char *t = ctime(&now);
-    fputs(t, stdout);
-    sleep(1);
+    int avail_mem = get_avail_memory();
+    if (avail_mem < MIN_MEM_THRES_IN_MIB) {
+      scream();
+    }
+
+    sleep(60);
   }
 
   return EXIT_SUCCESS;
